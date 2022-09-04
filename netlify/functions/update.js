dca374cf6e1ef365b9d3d3664be3a1604047f55c
Mikhail Borisov <mborisov@fenix.group>
2022-09-04 22:30:18 +0200

    from telegraf.js tutorial test

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,12 +1,24 @@
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
-exports.handler = async event => {
-  console.log('Received an update from Telegram!', event.body);
+// exports.handler = async event => {
+//   console.log('Received an update from Telegram!', event.body);
 
-  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: 'Got your message!',
-  });
+//   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+//     chat_id: JSON.parse(event.body).message.chat.id,
+//     text: 'Got your message!',
+//   });
 
-  return { statusCode: 200 };
-};
+//   return { statusCode: 200 };
+// };
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
