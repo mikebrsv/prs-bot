4e73ec8d1f7225898aedbc9610d774aec78ae1fb
Mikhail Borisov <mborisov@fenix.group>
2022-09-04 21:53:36 +0200

    axios

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,10 @@
 exports.handler = async event => {
   console.log('Received an update from Telegram!', event.body);
+
+  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    chat_id: JSON.parse(event.body).message.chat.id,
+    text: 'Got your message!',
+  });
+
   return { statusCode: 200 };
 };
