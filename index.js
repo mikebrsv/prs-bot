18037dba483d1014ab26a8eed39010a19f718365
Mikhail Borisov <mborisov@fenix.group>
2022-09-04 21:31:57 +0200

    Commit 0

---
+++ index.js
@@ -0,0 +1,30 @@
+import { Telegraf } from 'telegraf';
+
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('5619135668:AAG58I5Bu87iMXicWY1awlbdqwhdDayaR74');
+
+bot.command('quit', ctx => {
+  ctx.telegram.leaveChat(ctx.message.chat.id);
+  ctx.leaveChat();
+});
+
+bot.on('text', ctx => {
+  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+  ctx.reply(`Hello ${ctx.state.role}`);
+});
+
+bot.on('callback_query', ctx => {
+  ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+  ctx.answerCbQuery();
+});
+
+bot.on('inline_query', ctx => {
+  const result = [];
+  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
+  ctx.answerInlineQuery(result);
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
